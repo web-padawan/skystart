ad15d3371eafa085e039be4b2976e1dd543d89c3
web-padawan <skamandr@yandex.ru>
2016-01-28 13:58:38 +0200

    [new] version 1.0.1

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@ var gulp          = require('gulp'),
     sass          = require('gulp-sass'),
     cssnano       = require('gulp-cssnano'),
     glob          = require('gulp-css-globbing'),
-    spritesmith   = require('gulp.spritesmith'),
+    plumber       = require('gulp-plumber'),
     postcss       = require('gulp-postcss'),
+    rename        = require('gulp-rename'),
+    spritesmith   = require('gulp.spritesmith'),
     autoprefixer  = require('autoprefixer'),
     browserSync   = require('browser-sync'),
-    rename        = require('gulp-rename'),
     path          = require('path');
 
 gulp.task('reload', function() {
@@ -45,6 +46,7 @@ gulp.task('sass', function() {
     .pipe(glob({
       extensions: ['.scss']
     }))
+    .pipe(plumber())
     .pipe(sass({
       outputStyle: 'expanded',
       includePaths: ['./sass', 'node_modules/susy/sass', 'node_modules/breakpoint-sass/stylesheets']
